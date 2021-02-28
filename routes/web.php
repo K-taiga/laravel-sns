09ea3bb107b9d63c3270d5ff7eb84efec1423aed
K-taiga <taigakatou0329@gmail.com>
2021-02-28 14:52:14 +0900

    add show

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 Auth::routes();
 Route::get('/','ArticleController@index')->name('articles.index');
-Route::resource('/articles','ArticleController')->except(['index'])->middleware('auth');
+// resource(create,store,destroy,edit,update)にauthをかける
+Route::resource('/articles','ArticleController')->except(['index','show'])->middleware('auth');
+// resouceの中でshowだけmiddlewareなしでアクセス可
+Route::resource('/articles','ArticleController')->only(['show']);
