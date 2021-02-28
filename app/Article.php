8b0f83cac2ec61b5dd71c6e1cab8ec62d0b445eb
K-taiga <taigakatou0329@gmail.com>
2021-02-28 17:41:20 +0900

    add isLikedBy for User Model

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -15,5 +16,21 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        // 関連するモデル名,中間テーブル名の順
+        // 第二引数を省略した場合、２つのモデル名のスネークケースになる(ex.article_user)
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+        // likesを呼びその先のUserモデルが返ってくる
+        // そこに渡ってきたUserの$user->idがあるかチェックしboolで返す
+         ?(bool)$this->likes->where('id', $user->id)->count()
+         :false;
     }
 }
