5e14bc453364ec731ee382a94183c8baa4a1c099
K-taiga <taigakatou0329@gmail.com>
2021-03-01 08:22:34 +0900

    add likes&unlike feature

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/','ArticleController@index')->name('articles.index');
 Route::resource('/articles','ArticleController')->except(['index','show'])->middleware('auth');
 // resouceの中でshowだけmiddlewareなしでアクセス可
 Route::resource('/articles','ArticleController')->only(['show']);
+Route::prefix('articles')->name('articles.')->group(function(){
+    Route::put('/{article}/like','ArticleController@like')->name('like')->middleware('auth');
+    Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
+});
