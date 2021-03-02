a8076078c33f41089e0f81b7b71f0c9ae683e0f3
K-taiga <taigakatou0329@gmail.com>
2021-03-03 08:37:23 +0900

    add tag show

--- app/Tag.php
+++ app/Tag.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Tag extends Model
 {
     protected $fillable = [
@@ -13,5 +13,10 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return '#' . $this->name;
+    }
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
     }
 }
