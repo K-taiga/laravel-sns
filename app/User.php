966fe4c74e74c0480f4d7bd7d8b9e0f16a47fa17
K-taiga <taigakatou0329@gmail.com>
2021-03-04 16:20:53 +0900

    add FollowButton vue

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -42,5 +43,18 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new PasswordResetNotification($token,new BareMail()));
+    }
+
+    public function followers(): BelongsToMany
+    {
+        // リレーション元のusersテーブルはfollowee_idと紐づく
+        // リレーション先のusersテーブルはfollower_idと紐づく
+        return $this->belongsToMany('App\User','follows','followee_id','follower_id')->withTimestamps();
+    }
+
+    public function isFollowedBy(?User $user): bool
+    {
+        // followsのリレーション先(folloer_id)にuserのidがあるか
+        return $user ? (bool)$this->followers->where('id', $user->id)->count() : false;
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
 import ArticleTagsInput from './components/ArticleTagsInput'
-import FollowButon from './components/FollowButton'
+import FollowButton from './components/FollowButton'
 
 const app = new Vue({
     el:'#app',
